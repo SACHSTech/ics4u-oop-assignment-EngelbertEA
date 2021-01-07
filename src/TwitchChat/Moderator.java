d8035aac0865e1781c5f2feef0638bf7d519f97a
Engelbert Aroozoo <engelbert.aroozoo21@ycdsbk12.ca>
2021-01-07 04:18:28 +0000

    Setting up Moderators

---
+++ src/TwitchChat/Moderator.java
@@ -0,0 +1,13 @@
+package TwitchChat;
+
+import TwitchChat.*;
+
+public class Moderator{
+  private int intAdBreak;
+  private int intTimeOut;
+  private String strStreamTitle;
+
+  public Moderator(String username, int Points, int Donations, int Bits, String Streamer, int Ad, int TimeOut, String StreamTitle) {
+    super()
+  }
+}
